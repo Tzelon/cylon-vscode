f335dbef699f7282aae3f13f6ec4a4e6031689fa
Tzelon Machluf <tzelon47@gmail.com>
2019-08-16 13:47:46 +0300

    first commit

---
+++ scripts/e2e.sh
@@ -0,0 +1,7 @@
+
+#!/usr/bin/env bash
+
+export CODE_TESTS_PATH="$(pwd)/lsp/client/out/test"
+export CODE_TESTS_WORKSPACE="$(pwd)/lsp/client/testFixture"
+
+node "$(pwd)/lsp/client/node_modules/vscode/bin/test"
